c39862842d92abf9a5878572304911d7d89e057c
BhavyaTeja <bhavyateja5555@gmail.com>
2023-12-12 14:51:08 +0530

    first commit

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const recipeRoute = require("./controller/recipeRoute");
 
 const app = express()
 
-app.use(cors({
-    origin: 'http://localhost:3000', 
-    credentials: true,
-}))
+app.use(cors())
 
 app.use(cookieParser())
 app.use(bodyParser.json())
